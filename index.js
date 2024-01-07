dc5d4506d839aa67b574d3f0a03d0102255b2cb8
chris ho <98445729+ho0527@users.noreply.github.com>
2024-01-07 16:46:23 +0800

    fix: google login del

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-function googlecallback(event){
-    let tokensplit=event.credential.split(".")
-    let tokenbase64=regexpreplace(regexpreplace(tokensplit[1],/-/g,"+"),/_/g,"/")
-    let token=JSON.parse(atob(tokenbase64))
-    oldajax("POST","/backend/chrisjudge/login/thirdpartylogin/google",JSON.stringify(token),[
-        ["Content-Type","application/x-www-form-urlencoded"]
-    ]).onload=function(){
-        let data=JSON.parse(this.responseText)
-        if(data["success"]){
-            alert("登入成功")
-            localStorage.setItem("chrisjudgetoken",data["data"]["token"])
-            localStorage.setItem("chrisjudgepermission",data["data"]["permission"])
-            if(parseInt(weblsget("chrisjudgepermission"))>=4){
-                location.href="admin.html"
-            }else{
-                location.href="user.html"
-            }
-        }else{
-            alert(data["data"])
-        }
-    }
-}
-
 docgetid("submit").onclick=function(){
-    oldajax("POST","/backend/chrisjudge/login",JSON.stringify({
-        "username": docgetid("username").value,
-        "password": docgetid("password").value
-    }),[
-        ["Content-Type","application/json"]
-    ]).onload=function(){
-        let data=JSON.parse(this.responseText)
+    ajax("POST","/backend/chrisjudge/login",function(event,data){
         if(data["success"]){
             alert("登入成功")
             localStorage.setItem("chrisjudgetoken",data["data"]["token"])
@@ -36,7 +7,12 @@ docgetid("submit").onclick=function(){
         }else{
             alert(data["data"])
         }
-    }
+    },JSON.stringify({
+        "username": docgetid("username").value,
+        "password": docgetid("password").value
+    }),[
+        ["Content-Type","application/json"]
+    ])
 }
 
 docgetid("signup").onclick=function(){
