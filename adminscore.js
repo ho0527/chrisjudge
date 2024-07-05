b1f93cf532bacd67d90577fda64fb711a3c855ff
chris960527 <98445729+ho0527@users.noreply.github.com>
2024-07-05 17:56:56 +0800

    new: 顯示更新
    updatetime->20240705
    分數列表可以再每10秒更新

--- adminscore.js
+++ adminscore.js
@@ -1,8 +1,78 @@
 let questionid
 
+function main(){
+    ajax("GET",AJAXURL+"getscorelist",function(event,data){
+        if(data["success"]){
+            let scorelist=data["data"]
+            let questionidlist=data["questionidlist"]
+            let maintableheadinnerhtml=`
+                <tr class="tr">
+                    <td class="td">user\\題目</td>
+            `
+
+            for(let i=0;i<questionidlist.length;i=i+1){
+                maintableheadinnerhtml=`
+                    ${maintableheadinnerhtml}
+                    <td class="td">${questionidlist[i]}</td>
+                `
+            }
+
+            docgetid("maintable").innerHTML=`
+                    ${maintableheadinnerhtml}
+                </tr>
+            `
+
+            for(let i=0;i<scorelist.length;i=i+1){
+                if(scorelist[i]["responselist"]){
+                    let maintableuserscoreinnerhtml=`
+                        <tr class="tr">
+                            <td class="td">${scorelist[i]["nickname"]}</td>
+                    `
+
+                    for(let j=0;j<scorelist[i]["responselist"].length;j=j+1){
+                        let result=scorelist[i]["responselist"][j]["result"]
+                        let resultcolor=""
+
+                        if(result=="SU"){
+                            resultcolor="green"
+                        }else if(result=="MF"){
+                            resultcolor="red"
+                        }else if(result=="RF"){
+                            resultcolor="red"
+                        }else if(result=="OT"){
+                            resultcolor="red"
+                        }
+
+                        maintableuserscoreinnerhtml=`
+                            ${maintableuserscoreinnerhtml}
+                            <td class="td" style="background: ${resultcolor}">${result}</td>
+                        `
+                    }
+
+                    docgetid("maintable").innerHTML=`
+                            ${docgetid("maintable").innerHTML}
+                            ${maintableuserscoreinnerhtml}
+                        </tr>
+                    `
+                }
+            }
+        }else{
+            alert(userdata["data"])
+        }
+    },null,[
+        ["Authorization","Bearer "+weblsget("chrisjudgetoken")]
+    ])
+}
+
 if(!isset(weblsget("chrisjudgetoken"))){
     location.href="/chrisjudge/"
 }
+
+main()
+
+setInterval(function(){
+    main()
+},10000)
 
 docgetid("title").onclick=function(){
     location.href="admin.html"
@@ -23,68 +93,5 @@ docgetid("score").onclick=function(){
 docgetid("log").onclick=function(){
     location.href="log.html"
 }
-
-ajax("GET",AJAXURL+"getscorelist",function(event){
-    let data=JSON.parse(event.responseText)
-    if(data["success"]){
-        let scorelist=data["data"]
-        let questionidlist=data["questionidlist"]
-        let maintableheadinnerhtml=`
-            <tr>
-                <td class="td">題目 userid</td>
-        `
-
-        for(let i=0;i<questionidlist.length;i=i+1){
-            maintableheadinnerhtml=`
-                ${maintableheadinnerhtml}
-                <td class="td">${questionidlist[i]}</td>
-            `
-        }
-
-        docgetid("maintable").innerHTML=`
-                ${maintableheadinnerhtml}
-            </tr>
-        `
-
-        for(let i=0;i<scorelist.length;i=i+1){
-            if(scorelist[i]["responselist"]){
-                let maintableuserscoreinnerhtml=`
-                    <tr>
-                        <td class="td">${scorelist[i]["nickname"]}</td>
-                `
-
-                for(let j=0;j<scorelist[i]["responselist"].length;j=j+1){
-                    let result=scorelist[i]["responselist"][j]["result"]
-                    let resultcolor=""
-
-                    if(result=="SU"){
-                        resultcolor="green"
-                    }else if(result=="MF"){
-                        resultcolor="red"
-                    }else if(result=="RF"){
-                        resultcolor="red"
-                    }else if(result=="OT"){
-                        resultcolor="red"
-                    }
-
-                    maintableuserscoreinnerhtml=`
-                        ${maintableuserscoreinnerhtml}
-                        <td class="td" style="background: ${resultcolor}">${result}</td>
-                    `
-                }
-
-                docgetid("maintable").innerHTML=`
-                        ${docgetid("maintable").innerHTML}
-                        ${maintableuserscoreinnerhtml}
-                    </tr>
-                `
-            }
-        }
-    }else{
-        alert(userdata["data"])
-    }
-},null,[
-    ["Authorization","Bearer "+weblsget("chrisjudgetoken")]
-])
 
 startmacossection()
--- score.js
+++ score.js
@@ -1,8 +1,78 @@
 let questionid
 
+function main(){
+    ajax("GET",AJAXURL+"getscorelist",function(event,data){
+        if(data["success"]){
+            let scorelist=data["data"]
+            let questionidlist=data["questionidlist"]
+            let maintableheadinnerhtml=`
+                <tr class="tr">
+                    <td class="td">user\\題目</td>
+            `
+
+            for(let i=0;i<questionidlist.length;i=i+1){
+                maintableheadinnerhtml=`
+                    ${maintableheadinnerhtml}
+                    <td class="td">${questionidlist[i]}</td>
+                `
+            }
+
+            docgetid("maintable").innerHTML=`
+                    ${maintableheadinnerhtml}
+                </tr>
+            `
+
+            for(let i=0;i<scorelist.length;i=i+1){
+                if(scorelist[i]["responselist"]){
+                    let maintableuserscoreinnerhtml=`
+                        <tr class="tr">
+                            <td class="td">${scorelist[i]["nickname"]}</td>
+                    `
+
+                    for(let j=0;j<scorelist[i]["responselist"].length;j=j+1){
+                        let result=scorelist[i]["responselist"][j]["result"]
+                        let resultcolor=""
+
+                        if(result=="SU"){
+                            resultcolor="green"
+                        }else if(result=="MF"){
+                            resultcolor="red"
+                        }else if(result=="RF"){
+                            resultcolor="red"
+                        }else if(result=="OT"){
+                            resultcolor="red"
+                        }
+
+                        maintableuserscoreinnerhtml=`
+                            ${maintableuserscoreinnerhtml}
+                            <td class="td" style="background: ${resultcolor}">${result}</td>
+                        `
+                    }
+
+                    docgetid("maintable").innerHTML=`
+                            ${docgetid("maintable").innerHTML}
+                            ${maintableuserscoreinnerhtml}
+                        </tr>
+                    `
+                }
+            }
+        }else{
+            alert(userdata["data"])
+        }
+    },null,[
+        ["Authorization","Bearer "+weblsget("chrisjudgetoken")]
+    ])
+}
+
 if(!isset(weblsget("chrisjudgetoken"))){
     location.href="/chrisjudge/"
 }
+
+main()
+
+setInterval(function(){
+    main()
+},10000)
 
 docgetid("title").onclick=function(){
     location.href="user.html"
@@ -19,68 +89,5 @@ docgetid("profile").onclick=function(){
 docgetid("score").onclick=function(){
     location.reload()
 }
-
-ajax("GET",AJAXURL+"getscorelist",function(event){
-    let data=JSON.parse(event.responseText)
-    if(data["success"]){
-        let scorelist=data["data"]
-        let questionidlist=data["questionidlist"]
-        let maintableheadinnerhtml=`
-            <tr class="tr">
-                <td class="td">user\\題目</td>
-        `
-
-        for(let i=0;i<questionidlist.length;i=i+1){
-            maintableheadinnerhtml=`
-                ${maintableheadinnerhtml}
-                <td class="td">${questionidlist[i]}</td>
-            `
-        }
-
-        docgetid("maintable").innerHTML=`
-                ${maintableheadinnerhtml}
-            </tr>
-        `
-
-        for(let i=0;i<scorelist.length;i=i+1){
-            if(scorelist[i]["responselist"]){
-                let maintableuserscoreinnerhtml=`
-                    <tr class="tr">
-                        <td class="td">${scorelist[i]["nickname"]}</td>
-                `
-
-                for(let j=0;j<scorelist[i]["responselist"].length;j=j+1){
-                    let result=scorelist[i]["responselist"][j]["result"]
-                    let resultcolor=""
-
-                    if(result=="SU"){
-                        resultcolor="green"
-                    }else if(result=="MF"){
-                        resultcolor="red"
-                    }else if(result=="RF"){
-                        resultcolor="red"
-                    }else if(result=="OT"){
-                        resultcolor="red"
-                    }
-
-                    maintableuserscoreinnerhtml=`
-                        ${maintableuserscoreinnerhtml}
-                        <td class="td" style="background: ${resultcolor}">${result}</td>
-                    `
-                }
-
-                docgetid("maintable").innerHTML=`
-                        ${docgetid("maintable").innerHTML}
-                        ${maintableuserscoreinnerhtml}
-                    </tr>
-                `
-            }
-        }
-    }else{
-        alert(userdata["data"])
-    }
-},null,[
-    ["Authorization","Bearer "+weblsget("chrisjudgetoken")]
-])
 
 startmacossection()
